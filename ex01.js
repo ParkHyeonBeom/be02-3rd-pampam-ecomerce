b09d07e2b13c62e3d42c976b36ad5d7cafa2962f
Park Hyeon Beom <gusqja0213@naver.com>
2024-02-20 16:46:39 +0900

    test02

--- ex01.js
+++ ex01.js
@@ -6,14 +6,13 @@ const {Builder,By} = require('selenium-webdriver');
         .forBrowser('chrome')
         .build();
 
-        await driver.get('https://nid.naver.com/nidlogin.login?mode=form&url=https://www.naver.com/');
+        await driver.get('http://15.164.166.94:1234/member/login');
 
-        const input_id = await driver.findElement(By.id('id'));
+        const input_id = await driver.findElement(By.id('이메일'));
         input_id.sendKeys('gusqja0213');
 
-        const input_pw = await driver.findElement(By.id('pw'));
+        const input_pw = await driver.findElement(By.id('비밀번호'));
         input_pw.sendKeys('dydehf0213@');
 
-        const login_btn = await driver.findElement(By.id('log.login'));
-        login_btn.click();
+        driver.findElement(By.xpath("//button[contains(text(), '로그인')]")).click();
     })();
